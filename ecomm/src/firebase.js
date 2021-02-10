fce6198c773232bba3a42dbdaaa4b69271f93e96
Rob Moore <moore.re@gmail.com>
2021-02-10 14:35:04 -0600

    Started a popular site clone to prep for other projects

---
+++ ecomm/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJxTRXuJ675BAFPzdhv8ocPPnSveNxQVY",
+  authDomain: "ecomm-2c63c.firebaseapp.com",
+  projectId: "ecomm-2c63c",
+  storageBucket: "ecomm-2c63c.appspot.com",
+  messagingSenderId: "169654318882",
+  appId: "1:169654318882:web:8ed88f7432b25d5b94177b",
+  measurementId: "G-JHBV0R1PN9"
+};
