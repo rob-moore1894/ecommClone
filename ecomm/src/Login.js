8628efdd59937af0e6d6550a00dc2f80347f21c8
Rob Moore <moore.re@gmail.com>
2021-02-12 10:54:17 -0600

    Completed Login Front End feature

--- ecomm/src/Login.js
+++ ecomm/src/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const signIn = e => {
+        e.preventDefault();
+    }
+
+    const register = e => {
+        e.preventDefault();
+    }
+
     return(
         <div className="login">
             <Link to="/">
@@ -12,8 +23,17 @@ function Login(){
             <div className="login__container">
                 <h1>Sign In</h1>
                 <form>
-                    <h5></h5>
+                    <h5>Email</h5>
+                    <input type="text" value = {email} onChange={e => setEmail(e.target.value)}/>
+                    <h5>Password</h5>
+                    <input type="password" value = {password} onChange={e => setPassword(e.target.value)}/>
+                    <button className="login__signInButton" onClick={signIn} type='submit'>Sign In</button>
                 </form>
+                <p>
+                    By signing-in you agree to the FAKE AMAZON Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button className="login__registerButton" onClick={register}>Create an Account</button>
             </div>
         </div>
     )
